30c6f515b8ffc16b4a99c01043455c4e18bfdb99
saini1998 <sainiaaryaman1998@gmail.com>
2021-09-01 14:09:20 +0100

    Initial Commit

---
+++ ImarticusTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImarticusTestApp
+//
+//  Created by Aaryaman Saini on 01/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImarticusTestApp/ImarticusTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImarticusTestAppApp.swift
+//  ImarticusTestApp
+//
+//  Created by Aaryaman Saini on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ImarticusTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
